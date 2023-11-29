64c4729afa5c6f3b74a3cbbcb4e1a32eb953830c
coordova <grcordova@gmail.com>
2023-11-29 16:25:12 -0400

    Hotely - 02 Hero & SearchBox Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
-	return <main>Home Page</main>;
+	return (
+		<main className="max-w-[1440px] mx-auto bg-white overflow-hidden">
+			<Header />
+			<Hero />
+			<Footer />
+			<div className="h-[4000px]"></div>
+		</main>
+	);
 }
